ed23e1ad15523a56e0d554babc05dca308100e5f
Frank Rosner <frank@fam-rosner.de>
2015-01-04 10:30:38 +0100

    remove unnecessary pair generation step

--- src/de/frosner/ngrams/NGrams.scala
+++ src/de/frosner/ngrams/NGrams.scala
@@ -8,9 +8,9 @@ object NGrams {
 
   def main(args: Array[String]) {
     val n = args(0).toInt
-    val nGramLines = for (line <- Source.stdin.getLines()) yield line.sliding(n).map((_, 1))
+    val nGramLines = for (line <- Source.stdin.getLines()) yield line.sliding(n)
     val nGrams = nGramLines.reduce(_ ++ _).toIterable.par
-    val counts = nGrams.groupBy{ case (ngram, _) => ngram }.mapValues(_.size)
+    val counts = nGrams.groupBy(nGram => nGram).mapValues(_.size)
     counts foreach println
   }
 
